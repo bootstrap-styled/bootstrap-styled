a09db75f8674e4fc857aa32bfde49163b3774a66
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-01-30 12:02:39 +0700

    option updated

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -2,37 +2,19 @@
 
 import React, { PropTypes } from 'react';
 import styled from 'styled-components';
-import cn from 'classnames';
 
 class Option extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
-    className: PropTypes.string,
     children: PropTypes.node.isRequired,
-    selected: PropTypes.bool,
-    label: PropTypes.string,
-    value: PropTypes.string,
-    disabled: PropTypes.bool,
   };
 
   render() {
-    const { className, disabled, children, selected, label, value, ...rest } = this.props;
+    const { children, ...rest } = this.props;
 
-    const cssClasses = cn(className, {
-      selected,
-      label,
-      value,
-      disabled,
-    });
 
     return (
-      <option
-        className={cssClasses}
-        selected={selected}
-        label={label}
-        value={value}
-        {...rest}
-      >
+      <option {...rest}>
         {children}
       </option>
     );
