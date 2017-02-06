4bd123c0e73096d7735b4f206b57716f96f7d921
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2017-02-06 22:14:45 +0700

    remove some mixins and utilities to the APi

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,5 +1,5 @@
 
-import { A, Button, alertMixins, aUtils } from '../index';
+import { A, Button, BootstrapProvider, breakpointsMixins, visibilityUtils } from '../index';
 
 describe('api exports', () => {
   it('should have <A /> available', () => {
@@ -8,10 +8,13 @@ describe('api exports', () => {
   it('should have <Button /> available', () => {
     expect(Button).toBeDefined();
   });
-  it('should have alert mixins available', () => {
-    expect(alertMixins).toBeDefined();
+  it('should have <BootstrapProvider /> available', () => {
+    expect(BootstrapProvider).toBeDefined();
   });
-  it('should have a utilities available', () => {
-    expect(aUtils).toBeDefined();
+  it('should have breakpoint mixins available', () => {
+    expect(breakpointsMixins).toBeDefined();
+  });
+  it('should have visibility utilities available', () => {
+    expect(visibilityUtils).toBeDefined();
   });
 });
