90ce52d42facd77be34fdcd071fb9f902e80ee9a
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-02-10 16:37:13 +0700

    updated Td component so that i tcan take props such as colspan

--- src/components/Td/index.js
+++ src/components/Td/index.js
@@ -1,11 +1,40 @@
 /**
  * A Table cell
  */
+
+import React, { PropTypes } from 'react';
 import styled from 'styled-components';
+import bsTheme from 'theme';
+
 import { getBackgroundUtilities } from '../../styled/utilities/background';
 
-const Td = styled.td`
+const defaultProps = { theme: bsTheme };
+
+class Td extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    theme: PropTypes.object,
+  }
+
+  render() {
+    const { className, theme, children, ...rest } = this.props; // eslint-disable-line no-unused-vars
+    return (
+      <td className={className} {...rest}>
+        {children}
+      </td>
+    );
+  }
+
+}
+
+// eslint-disable-next-line no-class-assign
+Td = styled(Td)`
   ${getBackgroundUtilities()}
 `;
 
+Td.defaultProps = defaultProps;
+
 export default Td;
+
--- src/components/Td/tests/index.test.js
+++ src/components/Td/tests/index.test.js
@@ -1,25 +1,46 @@
 /**
  * Testing our Td component
  */
+import { ThemeProvider } from 'styled-components';
 
+import { mount } from 'enzyme';
 import React from 'react';
-import { shallow } from 'enzyme';
-import Td from '../index';
+import theme from 'theme';
 
-const children = <span> test </span>;
-const renderComponent = () => shallow(
-  <Td>
-    {children}
-  </Td>
+import Td from '../index';
+import Table from '../../Table';
+import Tr from '../../Tr';
+import Tbody from '../../Tbody';
+
+const children = (<h1>Test</h1>);
+
+const renderComponentUsingTheme = (props = {}) => mount(
+  <ThemeProvider theme={theme}>
+    <Table>
+      <Tbody>
+        <Tr>
+          <Td
+            className={props.className}
+          >
+            {props.children}
+          </Td>
+        </Tr>
+      </Tbody>
+    </Table>
+  </ThemeProvider>
 );
 
+
 describe('<Td />', () => {
-  it('should render a <Td> tag without a theme', () => {
-    const renderedComponent = renderComponent();
+  it('should render a <td> tag with a theme', () => {
+    const renderedComponent = renderComponentUsingTheme({
+      children,
+    });
     expect(renderedComponent.find('td').length).toBe(1);
+    expect(renderedComponent.find('Td').length).toBe(1);
   });
-  it('should have children without a theme', () => {
-    const renderedComponent = renderComponent({
+  it('should have children with a theme', () => {
+    const renderedComponent = renderComponentUsingTheme({
       children,
     });
     expect(renderedComponent.contains(children)).toEqual(true);
