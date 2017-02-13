dac2a7461a4183685fb4f09e22476bb71b65e0fd
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-02-13 16:14:30 +0700

    Deleted css that should not be in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,23 +5,10 @@
  */
 
 import styled from 'styled-components';
-import theme from 'theme';
-
-const defaultProps = { theme };
 
 const Footer = styled.footer`
-  ${(props) => `
-    height: ${props.theme['$footer-height']};
-    background: ${props.theme['$footer-background']};
-    width: 100%;
-    position: static;
-    left: 0px;
-    bottom: 0px;
-    text-align: center;
-    color: ${props.theme['$footer-color']};
-  `}
+
 `;
 
-Footer.defaultProps = defaultProps;
 
 export default Footer;
