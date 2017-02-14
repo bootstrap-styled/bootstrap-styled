0a384bda8f3076d39afd4b8b86e15cbfa60e1b33
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-02-14 11:37:55 +0700

    rectified jumbotron

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,21 +4,30 @@
 
 import React, { PropTypes } from 'react';
 import styled from 'styled-components';
-
+import cn from 'classnames';
+import bsTheme from 'theme';
 import { borderRadius } from '../../styled/mixins/border-radius';
 import { mediaBreakpointUp } from '../../styled/mixins/breakpoints';
+
+const defaultProps = {
+  theme: bsTheme,
+};
 
 class Jumbotron extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
     className: PropTypes.string,
     children: PropTypes.node,
+    theme: PropTypes.object,
   }
 
   render() {
-    const { className, children } = this.props;
+    const { className, children, theme, ...rest } = this.props; // eslint-disable-line no-unused-vars
     return (
-      <div className={className}>
+      <div
+        className={cn(className, 'jumbotron')}
+        {...rest}
+      >
         {children}
       </div>
     );
@@ -39,14 +48,14 @@ Jumbotron = styled(Jumbotron)`
     
       ${mediaBreakpointUp('sm',
         props.theme['$grid-breakpoints'],
-        `padding: calc(${props.theme['$jumbotron-padding']} * 2);`,
+        `padding: calc(${props.theme['$jumbotron-padding']} * 2) ${props.theme['$jumbotron-padding']};`,
       )}
 
     & .jumbotron-hr {
       border-top-color: darken(${props.theme['$jumbotron-bg']}, 10%);
     }
     
-    & .jumbotron-fluid {
+    &.jumbotron-fluid {
       padding-right: 0;
       padding-left: 0;
       ${borderRadius(
@@ -57,4 +66,5 @@ Jumbotron = styled(Jumbotron)`
   `}
 `;
 
+Jumbotron.defaultProps = defaultProps;
 export default Jumbotron;
