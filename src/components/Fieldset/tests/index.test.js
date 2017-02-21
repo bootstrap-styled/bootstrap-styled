b91ba20266b1a6a5762002e29f2b48f972ad7ac0
agd <agd@kopaxgroup.com>
2017-02-21 18:16:43 +0700

    IMPROVE fieldset component

--- src/components/Fieldset/tests/index.test.js
+++ src/components/Fieldset/tests/index.test.js
@@ -3,25 +3,13 @@
  */
 import { ThemeProvider } from 'styled-components';
 
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import theme from 'theme';
 
 import Fieldset from '../index';
 
 const children = (<h1>Test</h1>);
-
-const renderComponent = (props = {}) => shallow(
-  <Fieldset
-    disabled={props.disabled}
-    form={props.form}
-    name={props.name}
-    className={props.className}
-  >
-    {props.children}
-  </Fieldset>
-);
-
 
 const renderComponentUsingTheme = (props = {}) => mount(
   <ThemeProvider theme={theme}>
@@ -36,41 +24,7 @@ const renderComponentUsingTheme = (props = {}) => mount(
   </ThemeProvider>
 );
 
-
 describe('<Fieldset />', () => {
-  it('should render an <Fieldset> tag without a theme', () => {
-    const renderedComponent = renderComponent({
-      children,
-    });
-    expect(renderedComponent.find('fieldset').length).toBe(1);
-  });
-  it('should have an attribute disabled without a theme', () => {
-    const renderedComponent = renderComponent({
-      children,
-      disabled: true,
-    });
-    expect(renderedComponent.props().disabled).toEqual(true);
-  });
-  it('should have an attribute disabled without a theme', () => {
-    const renderedComponent = renderComponent({
-      children,
-      form: 'form_1',
-    });
-    expect(renderedComponent.props().form).toEqual('form_1');
-  });
-  it('should have an attribute name without a theme', () => {
-    const renderedComponent = renderComponent({
-      children,
-      name: 'name_1',
-    });
-    expect(renderedComponent.props().name).toEqual('name_1');
-  });
-  it('should have children without a theme', () => {
-    const renderedComponent = renderComponent({
-      children,
-    });
-    expect(renderedComponent.contains(children)).toEqual(true);
-  });
   it('should render a <fieldset> tag with a theme', () => {
     const renderedComponent = renderComponentUsingTheme({
       children,
