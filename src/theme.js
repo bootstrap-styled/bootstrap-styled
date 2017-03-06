65678b3f3a8914c2f0d4a5db80d51e616a8992cd
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2017-03-06 15:53:13 +0700

    uncomment modal props and new dist

--- src/theme.js
+++ src/theme.js
@@ -756,32 +756,32 @@ export function makeTheme(userTheme = {}) {
   // Modals
 
   // Padding applied to the modal body
-  // v['$modal-inner-padding'] = u['$modal-inner-padding'] || '15px';
-  //
-  // v['$modal-dialog-margin'] = u['$modal-dialog-margin'] || '10px';
-  // v['$modal-dialog-sm-up-margin-y'] = u['$modal-dialog-sm-up-margin-y'] || '30px';
-  //
-  // v['$modal-title-line-height'] = u['$modal-title-line-height'] || v['$line-height-base'];
-  //
-  // v['$modal-content-bg'] = u['$modal-content-bg'] || v['$white'];
-  // v['$modal-content-border-color'] = u['$modal-content-border-color'] || Color(v['$black']).alpha(0.2).toString();
-  // v['$modal-content-border-width'] = u['$modal-content-border-width'] || v['$border-width'];
-  // v['$modal-content-xs-box-shadow'] = u['$modal-content-xs-box-shadow'] || `0 3px 9px ${Color(v['$black']).alpha(0.5).toString()}`;
-  // v['$modal-content-sm-up-box-shadow'] = u['$modal-content-sm-up-box-shadow'] || `0 5px 15px ${Color(v['$black']).alpha(0.5).toString()}`;
-  //
-  // v['$modal-backdrop-bg'] = u['$modal-backdrop-bg'] || v['$black'];
-  // v['$modal-backdrop-opacity'] = u['$modal-backdrop-opacity'] || '.5';
-  // v['$modal-header-border-color'] = u['$modal-header-border-color'] || v['$gray-lighter'];
-  // v['$modal-footer-border-color'] = u['$modal-footer-border-color'] || v['$modal-header-border-color'];
-  // v['$modal-header-border-width'] = u['$modal-header-border-width'] || v['$modal-content-border-width'];
-  // v['$modal-footer-border-width'] = u['$modal-footer-border-width'] || v['$modal-header-border-width'];
-  // v['$modal-header-padding'] = u['$modal-header-padding'] || '15px';
-  //
-  // v['$modal-lg'] = u['$modal-lg'] || '800px';
-  // v['$modal-md'] = u['$modal-md'] || '500px';
-  // v['$modal-sm'] = u['$modal-sm'] || '300px';
-  //
-  // v['$modal-transition'] = u['$modal-transition'] || 'transform .3s ease-out';
+  v['$modal-inner-padding'] = u['$modal-inner-padding'] || '15px';
+
+  v['$modal-dialog-margin'] = u['$modal-dialog-margin'] || '10px';
+  v['$modal-dialog-sm-up-margin-y'] = u['$modal-dialog-sm-up-margin-y'] || '30px';
+
+  v['$modal-title-line-height'] = u['$modal-title-line-height'] || v['$line-height-base'];
+
+  v['$modal-content-bg'] = u['$modal-content-bg'] || v['$white'];
+  v['$modal-content-border-color'] = u['$modal-content-border-color'] || Color(v['$black']).alpha(0.2).toString();
+  v['$modal-content-border-width'] = u['$modal-content-border-width'] || v['$border-width'];
+  v['$modal-content-xs-box-shadow'] = u['$modal-content-xs-box-shadow'] || `0 3px 9px ${Color(v['$black']).alpha(0.5).toString()}`;
+  v['$modal-content-sm-up-box-shadow'] = u['$modal-content-sm-up-box-shadow'] || `0 5px 15px ${Color(v['$black']).alpha(0.5).toString()}`;
+
+  v['$modal-backdrop-bg'] = u['$modal-backdrop-bg'] || v['$black'];
+  v['$modal-backdrop-opacity'] = u['$modal-backdrop-opacity'] || '.5';
+  v['$modal-header-border-color'] = u['$modal-header-border-color'] || v['$gray-lighter'];
+  v['$modal-footer-border-color'] = u['$modal-footer-border-color'] || v['$modal-header-border-color'];
+  v['$modal-header-border-width'] = u['$modal-header-border-width'] || v['$modal-content-border-width'];
+  v['$modal-footer-border-width'] = u['$modal-footer-border-width'] || v['$modal-header-border-width'];
+  v['$modal-header-padding'] = u['$modal-header-padding'] || '15px';
+
+  v['$modal-lg'] = u['$modal-lg'] || '800px';
+  v['$modal-md'] = u['$modal-md'] || '500px';
+  v['$modal-sm'] = u['$modal-sm'] || '300px';
+
+  v['$modal-transition'] = u['$modal-transition'] || 'transform .3s ease-out';
 
 
   // Alerts
