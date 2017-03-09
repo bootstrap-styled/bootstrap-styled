5cdf16e56bfc74a73e02ef9c2882e0b841f0170f
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-03-09 21:40:26 +0700

    improved Col folowing ReactStrap

--- src/components/Col/index.js
+++ src/components/Col/index.js
@@ -1,22 +1,106 @@
 import styled from 'styled-components';
 import React, { PropTypes } from 'react';
-import theme from 'theme';
+import cn from 'classnames';
+import themeBs from 'theme';
 import { makeGridColumns } from '../../styled/mixins/grid-framework';
+import { mapToCssModules } from '../../styled/utilities/tools';
 
-const defaultProps = { theme };
+const colWidths = ['xs', 'sm', 'md', 'lg', 'xl'];
+const stringOrNumberProp = PropTypes.oneOfType([PropTypes.number, PropTypes.string]);
+
+const columnProps = PropTypes.oneOfType([
+  PropTypes.bool,
+  PropTypes.number,
+  PropTypes.string,
+  PropTypes.shape({
+    size: PropTypes.oneOfType([PropTypes.bool, PropTypes.number, PropTypes.string]),
+    push: stringOrNumberProp,
+    pull: stringOrNumberProp,
+    offset: stringOrNumberProp,
+  }),
+]);
+
+const defaultProps = {
+  theme: themeBs,
+  tag: 'div',
+  widths: colWidths,
+};
+
+const getColumnSizeClass = (isXs, colWidth, colSize) => {
+  if (colSize === true || colSize === '') {
+    return isXs ? 'col' : `col-${colWidth}`;
+  } else if (colSize === 'auto') {
+    return isXs ? 'col-auto' : `col-${colWidth}-auto`;
+  }
+
+  return isXs ? `col-${colSize}` : `col-${colWidth}-${colSize}`;
+};
 
 class Col extends React.Component {    // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    xs: columnProps,
+    sm: columnProps,
+    md: columnProps,
+    lg: columnProps,
+    xl: columnProps,
     className: PropTypes.string,
-    children: PropTypes.node.isRequired,
+    cssModule: PropTypes.object,
+    widths: PropTypes.array,
+    theme: PropTypes.object,
   }
 
   render() {
+    const {
+      className,
+      cssModule,
+      widths,
+      tag: Tag,
+      theme,  // eslint-disable-line
+      ...attributes
+    } = this.props;
+    const colClasses = [];
+
+    widths.forEach((colWidth, i) => {
+      let columnProp = this.props[colWidth];
+
+      if (!i && columnProp === undefined) {
+        columnProp = true;
+      }
+
+      delete attributes[colWidth];
+
+      if (!columnProp) {
+        return;
+      }
+
+      const isXs = !i;
+      let colClass;
+
+      if (typeof columnProp === 'object') {
+        const colSizeInterfix = isXs ? '-' : `-${colWidth}-`;
+        colClass = getColumnSizeClass(isXs, colWidth, columnProp.size);
+
+        colClasses.push(mapToCssModules(cn({
+          [colClass]: columnProp.size || columnProp.size === '',
+          [`push${colSizeInterfix}${columnProp.push}`]: columnProp.push,
+          [`pull${colSizeInterfix}${columnProp.pull}`]: columnProp.pull,
+          [`offset${colSizeInterfix}${columnProp.offset}`]: columnProp.offset,
+        })), cssModule);
+      } else {
+        colClass = getColumnSizeClass(isXs, colWidth, columnProp);
+        colClasses.push(colClass);
+      }
+    });
+
+    const classes = mapToCssModules(cn(
+      className,
+      colClasses
+    ), cssModule);
+
     return (
-      <div className={this.props.className}>
-        {this.props.children}
-      </div>
+      <Tag {...attributes} className={classes} />
     );
   }
 }
