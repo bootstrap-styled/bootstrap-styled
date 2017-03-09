bf2272410701f4e88a4035b06d8b206525195ef0
agd <agd@kopaxgroup.com>
2017-03-09 13:16:41 +0700

    Create Forms components: Form, FormGroup, FormText, FormFeedback

---
+++ src/components/Forms/FormFeedback.js
@@ -0,0 +1,45 @@
+/* eslint-disable quote-props, dot-notation */
+/**
+ * Form Feedback Component test
+ *
+ *
+ */
+import React, { PropTypes } from 'react';
+import cn from 'classnames';
+import { mapToCssModules } from '../../styled/utilities/tools';
+
+const defaultProps = {
+  tag: 'div',
+};
+
+class FormFeedback extends React.Component {// eslint-disable-line react/prefer-stateless-function
+
+  static propTypes = {
+    children: PropTypes.node,
+    tag: PropTypes.string,
+    className: PropTypes.string,
+    cssModule: PropTypes.object,
+  }
+
+  render() {
+    const {
+      className,
+      cssModule,
+      tag: Tag,
+      ...attributes
+    } = this.props;
+
+    const classes = mapToCssModules(cn(
+      className,
+      'form-control-feedback'
+    ), cssModule);
+
+    return (
+      <Tag {...attributes} className={classes} />
+    );
+  }
+}
+
+FormFeedback.defaultProps = defaultProps;
+
+export default FormFeedback;
