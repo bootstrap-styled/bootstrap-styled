70a75b390f735213558322aa0ab6e16cf751b05f
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-03-10 15:41:05 +0700

    changed Alert test following componenet update

--- src/components/Alert/tests/index.test.js
+++ src/components/Alert/tests/index.test.js
@@ -37,27 +37,10 @@ describe('<Alert />', () => {
     });
     expect(renderedComponent.find('div').hasClass('alert')).toBe(true);
   });
-  // it.only('should have a onDismiss attribute without a theme', () => {
-  //   const renderedComponent = renderComponentUsingTheme({
-  //     children,
-  //     store,
-  //     onDismiss: this.hideError,
-  //   });
-  //   console.log(renderedComponent.debug());
-  //   expect(renderedComponent.find('div').hasClass('alert-dismissible')).toBe(true);
-  // });
   it('should have children with a theme', () => {
     const renderedComponent = renderComponentUsingTheme({
       children,
     });
     expect(renderedComponent.contains(children)).toEqual(true);
-  });
-  it('should render close button', () => {
-    const renderedComponent = renderComponentUsingTheme({
-      children,
-      close: true,
-      dismissed: false,
-    });
-    expect(renderedComponent.find('Close').length).toEqual(2);
   });
 });
