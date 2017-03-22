4cbf18c4d041225bfea196798e1faeb66cd97b43
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-03-23 00:26:39 +0700

    added missing arguments to buttonGroup mixin

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -55,21 +55,21 @@ class ButtonGroup extends React.Component { // eslint-disable-line react/prefer-
 ButtonGroup = styled(ButtonGroup)`
   ${(props) => `
     ${buttonGroup(
-  props.theme['$enable-shadows'],
-  props.theme['$enable-rounded'],
-  props.theme['$input-btn-border-width'],
-  props.theme['$btn-toolbar-margin'],
-  props.theme['$btn-padding-x'],
-  props.theme['$btn-active-box-shadow'],
-  props.theme['$btn-padding-x-lg'],
-  props.theme['$btn-padding-y-lg'],
-  props.theme['$font-size-lg'],
-  props.theme['$btn-border-radius-lg'],
-  props.theme['$btn-padding-x-sm'],
-  props.theme['$btn-padding-y-sm'],
-  props.theme['$font-size-sm'],
-  props.theme['$btn-border-radius-sm']
-)}
+      props.theme['$enable-shadows'],
+      props.theme['$enable-rounded'],
+      props.theme['$input-btn-border-width'],
+      props.theme['$btn-toolbar-margin'],
+      props.theme['$btn-padding-x'],
+      props.theme['$btn-active-box-shadow'],
+      props.theme['$btn-padding-x-lg'],
+      props.theme['$btn-padding-y-lg'],
+      props.theme['$font-size-lg'],
+      props.theme['$btn-border-radius-lg'],
+      props.theme['$btn-padding-x-sm'],
+      props.theme['$btn-padding-y-sm'],
+      props.theme['$font-size-sm'],
+      props.theme['$btn-border-radius-sm']
+    )}
   `}  
 `;
 
