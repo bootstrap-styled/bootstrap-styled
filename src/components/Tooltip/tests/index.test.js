e1ec766ab059f2f561022db9b952b31b6f07ead0
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2017-04-19 04:31:07 +0700

    improve tooltip test

--- src/components/Tooltip/tests/index.test.js
+++ src/components/Tooltip/tests/index.test.js
@@ -1,32 +1,36 @@
 /**
  * Testing our Tooltip component
  */
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
-
+import BootstrapProvider from '../../BootstrapProvider';
 import Tooltip from '../index';
 
-const children = (<h1>Test</h1>);
-
-const renderComponent = (props = {}) => shallow(
-  <Tooltip {...props}>
-    {props.children}
-  </Tooltip>
+const renderComponent = (props = {}) => mount(
+  <BootstrapProvider>
+    <div id="my-target"></div>
+    <Tooltip {...props} placement="top" />
+  </BootstrapProvider>
 );
 
 describe('<Tooltip />', () => {
-  it('should render an <Tooltip> tag without a theme', () => {
-    const renderedComponent = renderComponent({
+  let props;
+
+  beforeEach(() => {
+    props = {
+      children: <h1>Test</h1>,
       target: 'targetId',
-      children,
-    });
+    };
+  });
+
+  it('should render an <Tooltip> tag without a theme', () => {
+    const renderedComponent = renderComponent(props);
     expect(renderedComponent.find('Tooltip').length).toBe(1);
   });
   it('should have children without a theme', () => {
-    const renderedComponent = renderComponent({
-      target: 'targetId',
-      children,
-    });
-    expect(renderedComponent.contains(children)).toEqual(true);
+    const renderedComponent = shallow(
+      <Tooltip {...props} placement="top" />
+    );
+    expect(renderedComponent.contains(props.children)).toEqual(true);
   });
 });
