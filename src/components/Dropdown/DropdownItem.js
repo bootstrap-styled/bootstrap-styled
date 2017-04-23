c435f69c7805e027fb536c5141e1b0acceaa834a
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-04-23 12:08:44 +0700

    changed default tag value of DropdownItem to A

--- src/components/Dropdown/DropdownItem.js
+++ src/components/Dropdown/DropdownItem.js
@@ -2,8 +2,10 @@ import React, { PropTypes } from 'react';
 import cn from 'classnames';
 import { mapToCssModules } from 'utils/tools';
 import H6 from '../H6';
+import A from '../A';
+
 const defaultProps = {
-  tag: 'button',
+  tag: A,
 };
 
 class DropdownItem extends React.Component {
@@ -62,7 +64,7 @@ class DropdownItem extends React.Component {
       }
     ), cssModule);
 
-    if (Tag === 'button') {
+    if (Tag === A) {
       if (header) {
         Tag = H6;
       } else if (divider) {
