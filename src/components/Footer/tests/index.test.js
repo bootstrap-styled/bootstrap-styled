d0e1078c2a77b4e736bd40491a52c1e4c8462698
Alexander Janet <alexander.janet@kopaxgroup.com>
2017-04-26 02:29:21 +0700

    refractor Footer

--- src/components/Footer/tests/index.test.js
+++ src/components/Footer/tests/index.test.js
@@ -1,33 +1,23 @@
 /**
  * Testing our Footer component
  */
-import { ThemeProvider } from 'styled-components';
-
 import { shallow, mount } from 'enzyme';
 import React from 'react';
-import theme from 'theme';
+import BootstrapProvider from '../../BootstrapProvider';
 
 import Footer from '../index';
 
 const children = (<h1>Test</h1>);
 
 const renderComponent = (props = {}) => shallow(
-  <Footer
-    className={props.className}
-  >
-    {props.children}
-  </Footer>
+  <Footer {...props} />
 );
 
 
 const renderComponentUsingTheme = (props = {}) => mount(
-  <ThemeProvider theme={theme}>
-    <Footer
-      className={props.className}
-    >
-      {props.children}
-    </Footer>
-  </ThemeProvider>
+  <BootstrapProvider>
+    <Footer {...props} />
+  </BootstrapProvider>
 );
 
 
