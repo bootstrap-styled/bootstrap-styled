f67394d4762d63887e441cb655a3e1c1b5f41c34
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2017-04-29 04:24:12 +0700

    PropTypes correct package import

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import bsTheme from 'theme';
 import omit from 'lodash.omit';
