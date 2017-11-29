dd863cbf03586d2514bfcb39c0c6650ced63e74e
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2017-11-29 19:15:59 +0700

    Add new theme.js and theme.test.js to ListGroup, Li, Label, Kbd, Jumbotron, InputGroup, Input, Img, Hr and H1 to H6 components

---
+++ src/InputGroup/theme.js
@@ -0,0 +1,103 @@
+import Color from 'color';
+import unitUtils from 'bootstrap-styled-utils/lib/unitUtils';
+import { allowFalseValue } from '../makeTheme/utils';
+const { rmUnit, UNIT } = unitUtils;
+
+/**
+ * BOOTSTRAP THEME INPUTGROUP
+ *
+ * this file is not meant to be used and must be kept as original
+ * @param userTheme
+ */
+export const makeTheme = (userTheme = {}) => {
+  /* eslint dot-notation: 'off', new-cap: 'off' */
+  // Variables
+  //
+  // Copy settings from this file into the provided `_custom.scss` to override
+  // the Bootstrap defaults without modifying key, versioned files.
+  const v = {};
+  const u = userTheme;
+
+  // Options
+  //
+  // Quickly modify global styling by enabling or disabling optional features.
+
+  v['$enable-rounded'] = allowFalseValue(u['$enable-rounded'], true);
+  v['$enable-hover-media-query'] = allowFalseValue(u['$enable-hover-media-query'], false);
+  v['$enable-transitions'] = allowFalseValue(u['$enable-transitions'], true);
+  v['$enable-shadows'] = allowFalseValue(u['$enable-shadows'], false);
+
+  // Spacing
+
+  v['$border-width'] = u['$border-width'] || '1px';
+
+  // Colors
+  //
+  // Grayscale and brand colors for use across Bootstrap.
+
+  // Start with assigning color names to specific hex values.
+
+  v['$black'] = u['$black'] || '#000';
+  v['$blue'] = u['$blue'] || '#0275d8';
+
+  // Create grayscale
+
+  v['$gray'] = u['$gray'] || '#464a4c';
+  v['$gray-light'] = u['$gray-light'] || '#636c72';
+  v['$gray-lighter'] = u['$gray-lighter'] || '#eceeef';
+
+  // Reassign color vars to semantic color scheme
+  v['$brand-primary'] = u['$brand-primary'] || v['$blue'];
+
+  // Fonts
+  //
+  // Font, line-height, and color for body text, headings, and more.
+
+  v['$font-size-base'] = u['$font-size-base'] || '1rem'; // Assumes the browser default, typically `16px`
+  v['$font-size-lg'] = u['$font-size-lg'] || '1.25rem';
+  v['$font-size-sm'] = u['$font-size-sm'] || '.875rem';
+  v['$font-size-xs'] = u['$font-size-xs'] || '.75rem';
+  v['$line-height-base'] = u['$line-height-base'] || '1.5';
+
+  // Forms
+
+  v['$input-padding-x'] = u['$input-padding-x'] || '.75rem';
+  v['$input-padding-y'] = u['$input-padding-y'] || '.5rem';
+
+  v['$input-bg'] = u['$input-bg'] || v['$white'];
+  v['$input-bg-disabled'] = u['$input-bg-disabled'] || v['$gray-lighter'];
+
+  v['$input-padding-x-lg'] = u['$input-padding-x-lg'] || '1.5rem';
+  v['$input-padding-y-lg'] = u['$input-padding-y-lg'] || '.75rem';
+  v['$input-line-height'] = u['$input-line-height'] || '1.25';
+  v['$input-height'] = u['$input-height'] || ((rmUnit(v['$font-size-base'], UNIT.REM) * v['$line-height-base']) + (rmUnit(v['$input-padding-y'], UNIT.REM) * 2) + UNIT.REM);
+  v['$input-box-shadow'] = u['$input-box-shadow'] || `inset 0 1px 1px ${Color(v['$black']).alpha(0.075).toString()}`;
+
+  v['$input-btn-border-width'] = u['$input-btn-border-width'] || v['$border-width']; // For form controls and buttons
+
+  v['$input-group-addon-bg'] = u['$input-group-addon-bg'] || v['$gray-lighter'];
+  v['$input-border-color'] = u['$input-border-color'] || Color(v['$black']).alpha(0.15).toString();
+  v['$input-border-focus'] = u['$input-border-focus'] || Color(v['$brand-primary']).lighten(0.25).toString();
+  v['$input-box-shadow-focus'] = u['$input-box-shadow-focus'] || `${v['$input-box-shadow']}, 0 0 8px rgba(${v['$input-border-focus']},.6)`;
+
+  v['$input-color'] = u['$input-color'] || v['$gray'];
+  v['$input-group-addon-border-color'] = u['$input-group-addon-border-color'] || v['$input-border-color'];
+
+  v['$input-padding-x-sm'] = u['$input-padding-x-sm'] || '.5rem';
+  v['$input-padding-y-sm'] = u['$input-padding-y-sm'] || '.25rem';
+  v['$input-color-placeholder'] = u['$input-color-placeholder'] || v['$gray-light'];
+
+  v['$input-border-radius-lg'] = u['$input-border-radius-lg'] || v['$border-radius-lg'];
+  v['$input-border-radius-sm'] = u['$input-border-radius-sm'] || v['$border-radius-sm'];
+  v['$input-border-radius'] = u['$input-border-radius'] || v['$border-radius'];
+
+  v['$input-transition'] = u['$input-transition'] || 'border-color ease-in-out .15s, box-shadow ease-in-out .15s';
+  v['$input-color-focus'] = u['$input-color-focus'] || v['$input-color'];
+  v['$input-bg-focus'] = u['$input-bg-focus'] || v['$input-bg'];
+
+  v['$cursor-disabled'] = u['$cursor-disabled'] || 'not-allowed';
+
+  return Object.assign({}, u, v);
+};
+
+export default makeTheme();
---
+++ src/ListGroup/theme.js
@@ -0,0 +1,85 @@
+import Color from 'color';
+import { allowFalseValue } from '../makeTheme/utils';
+/**
+ * BOOTSTRAP THEME LISTGROUP
+ *
+ * this file is not meant to be used and must be kept as original
+ * @param userTheme
+ */
+export const makeTheme = (userTheme = {}) => {
+  /* eslint dot-notation: 'off', new-cap: 'off' */
+  // Variables
+  //
+  // Copy settings from this file into the provided `_custom.scss` to override
+  // the Bootstrap defaults without modifying key, versioned files.
+  const v = {};
+  const u = userTheme;
+
+  // Options
+  //
+  // Quickly modify global styling by enabling or disabling optional features.
+
+  v['$enable-rounded'] = allowFalseValue(u['$enable-rounded'], true);
+  v['$enable-hover-media-query'] = allowFalseValue(u['$enable-hover-media-query'], false);
+
+  // Colors
+  //
+  // Grayscale and brand colors for use across Bootstrap.
+
+  // Start with assigning color names to specific hex values.
+  v['$black'] = u['$black'] || '#000';
+
+  // Create grayscale
+  v['$gray-light'] = u['$gray-light'] || '#636c72';
+  v['$gray-lighter'] = u['$gray-lighter'] || '#eceeef';
+  v['$gray-lightest'] = u['$gray-lightest'] || '#f7f7f9';
+
+  // List group
+
+  v['$list-group-color'] = u['$list-group-color'] || v['$body-color'];
+  v['$list-group-bg'] = u['$list-group-bg'] || v['$white'];
+  v['$list-group-border-color'] = u['$list-group-border-color'] || Color(v['$black']).alpha(0.125).toString();
+  v['$list-group-border-width'] = u['$list-group-border-width'] || v['$border-width'];
+  v['$list-group-border-radius'] = u['$list-group-border-radius'] || v['$border-radius'];
+
+  v['$list-group-item-padding-x'] = u['$list-group-item-padding-x'] || '1.25rem';
+  v['$list-group-item-padding-y'] = u['$list-group-item-padding-y'] || '.75rem';
+
+  v['$list-group-hover-bg'] = u['$list-group-hover-bg'] || v['$gray-lightest'];
+  v['$list-group-active-color'] = u['$list-group-active-color'] || v['$component-active-color'];
+  v['$list-group-active-bg'] = u['$list-group-active-bg'] || v['$component-active-bg'];
+  v['$list-group-active-border'] = u['$list-group-active-border'] || v['$list-group-active-bg'];
+
+  v['$list-group-disabled-color'] = u['$list-group-disabled-color'] || v['$gray-light'];
+  v['$list-group-disabled-bg'] = u['$list-group-disabled-bg'] || v['$list-group-bg'];
+
+  v['$list-group-link-color'] = u['$list-group-link-color'] || v['$gray'];
+  v['$list-group-link-hover-color'] = u['$list-group-link-hover-color'] || v['$list-group-link-color'];
+
+  v['$list-group-link-active-color'] = u['$list-group-link-active-color'] || v['$list-group-color'];
+  v['$list-group-link-active-bg'] = u['$list-group-link-active-bg'] || v['$gray-lighter'];
+
+  // Forms
+
+  v['$cursor-disabled'] = u['$cursor-disabled'] || 'not-allowed';
+
+  // Form states and alerts
+  //
+  // Define colors for form feedback states and, by default, alerts.
+
+  v['$state-success-text'] = u['$state-success-text'] || '#3c763d';
+  v['$state-success-bg'] = u['$state-success-bg'] || '#dff0d8';
+
+  v['$state-info-text'] = u['$state-info-text'] || '#31708f';
+  v['$state-info-bg'] = u['$state-info-bg'] || '#d9edf7';
+
+  v['$state-warning-text'] = u['$state-warning-text'] || '#8a6d3b';
+  v['$state-warning-bg'] = u['$state-warning-bg'] || '#fcf8e3';
+
+  v['$state-danger-text'] = u['$state-danger-text'] || '#a94442';
+  v['$state-danger-bg'] = u['$state-danger-bg'] || '#f2dede';
+
+  return Object.assign({}, u, v);
+};
+
+export default makeTheme();
