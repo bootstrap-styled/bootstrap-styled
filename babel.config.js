b4fd2897f52ccf5186fc3b13f7d6b346a3f7cc2b
Gitlab-CI <dev@kopaxgroup.com>
2018-12-07 01:11:48 +0700

    fix(dependencies): Upgrade many deps, semantic-release, babel 7, remove color

--- babel.config.js
+++ babel.config.js
@@ -1,48 +1,42 @@
-{
-  "only": [
-    "src",
-    "styleguide"
+module.exports = {
+  only: [
+    'src',
+    'styleguide',
   ],
-  "presets": [
+  presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        "modules": false
-      }
+        modules: false,
+      },
     ],
-    "@babel/preset-react"
+    '@babel/preset-react',
   ],
-  "plugins": [
-    "array-includes",
-    "@babel/plugin-proposal-class-properties",
-    "styled-components",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-syntax-import-meta",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-json-strings",
+  plugins: [
+    'styled-components',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-json-strings',
     [
-      "@babel/plugin-proposal-decorators",
+      '@babel/plugin-proposal-decorators',
       {
-        "legacy": true
-      }
+        legacy: true,
+      },
     ],
-    "@babel/plugin-proposal-function-sent",
-    "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-numeric-separator",
-    "@babel/plugin-proposal-throw-expressions"
   ],
-  "env": {
-    "production": {
-      "plugins": [
-        "add-module-exports",
-        "@babel/plugin-transform-modules-commonjs"
-      ]
+  env: {
+    production: {
+      plugins: [
+        'add-module-exports',
+        '@babel/plugin-transform-modules-commonjs',
+      ],
     },
-    "test": {
-      "plugins": [
-        "@babel/plugin-transform-modules-commonjs",
-        "dynamic-import-node"
-      ]
-    }
-  }
-}
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'dynamic-import-node',
+      ],
+    },
+  },
+};
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,4 +1,4 @@
-import Color from 'color';
+import Color from '@bootstrap-styled/color';
 import unitUtils from '@bootstrap-styled/utils/lib/unitUtils';
 import { allowFalseValue, assertAscending, assertStartAtZero } from './utils';
 
